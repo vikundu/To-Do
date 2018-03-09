4d8ddd71758ddf741be85c2a6eb9f56156cb61fc
Vikas Kundu <kundu.vikas13@gmail.com>
2018-03-09 14:19:59 +0000

    Adding the update and delete functionality

--- public/app.js
+++ public/app.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
             createTodo();
         }
     });
+    
+    $('.list').on('click','li',function(){
+        updateTodo($(this));
+    });
+    
+    $('.list').on('click','span',function(event){
+        event.stopPropagation();
+        removeTodo($(this).parent());
+    });
 });
 
 function addTodos(todos){
@@ -17,11 +26,13 @@ function addTodos(todos){
 }
 
 function addTodo(todo){
-    var newTodo = $('<li class="task">'+todo.name+'</li>');
-        if(todo.completed){
-            newTodo.addClass("done");
-        }
-        $('.list').append(newTodo);
+    var newTodo = $('<li class="task">'+todo.name+'<span>X</span></li>');
+    newTodo.data('id',todo._id);
+    newTodo.data('completed',todo.completed);
+    if(todo.completed){
+        newTodo.addClass("done");
+    }
+    $('.list').append(newTodo);
 }
 
 function createTodo(){
@@ -33,5 +44,34 @@ function createTodo(){
     })
     .catch(function(err){
         console.log(err);
+    })
+}
+
+function removeTodo(todo){
+    var clickedId = todo.data('id');
+    var deleteUrl = '/api/todos/'+clickedId;
+    $.ajax({
+        method: 'DELETE',
+        url: deleteUrl
+    })
+    .then(function(data){
+        todo.remove();
+        
+    })
+}
+
+function updateTodo(todo){
+    var updateUrl = '/api/todos/'+todo.data('id');
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone};
+    $.ajax({
+        method: 'PUT',
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updatedTodo){
+        todo.toggleClass("done");
+        todo.data("completed",isDone);
+        console.log(updatedTodo);
     })
 }
